9ad83464990d9c35b40e3d1e9ab7a0d30dc5970c
weslei luiz de paula pinto <weslei.paula@fatec.sp.gov.br>
2019-02-22 10:34:26 -0300

    Modificações realizadas:
    * pom.xml parent criado com dependências principais do projeto
    * arquivos de configuração inseridos nos projetos filhos
    * poms atualizados para que a configuração do pom principal seja herdada
    * deploy já realizado, release versão 1.2.2

--- EsfingeGuardian-MAC/src/org/esfinge/guardian/mac/utils/MacConfig.java
+++ EsfingeGuardian-MAC/src/org/esfinge/guardian/mac/utils/MacConfig.java
@@ -1,12 +1,32 @@
 package org.esfinge.guardian.mac.utils;
 
-import java.util.ResourceBundle;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class MacConfig {
-
-	public static ResourceBundle bundle = ResourceBundle.getBundle("META-INF/MacConfig");
 	
-	static public String getAuthorizationLevelKey() {
-		return bundle.getString("authorizationLevel.key");
+	private Properties prop;
+
+	public MacConfig() {
+		prop = loadProperties();
 	}
+
+	public String getAuthorizationLevelKey() {
+		return prop.getProperty("authorizationLevel.key");
+	}
+
+	private Properties loadProperties() {
+		if (prop == null) {
+			prop = new Properties();
+			try {
+				prop.load(new FileInputStream("src/META-INF/services/MacConfig.properties"));
+			} catch (IOException e) {
+				Logger.getLogger(this.getClass().getName(), "MacConfig could not be loaded: " + e);
+			}
+		}
+		return prop;
+	}
+
 }
--- EsfingeGuardian-MAC/test/org/esfinge/guardian/mac/test/tester/SensitivityLevelTester.java
+++ EsfingeGuardian-MAC/test/org/esfinge/guardian/mac/test/tester/SensitivityLevelTester.java
@@ -11,69 +11,73 @@ import org.esfinge.guardian.mac.test.GuardedMACClass;
 import org.esfinge.guardian.mac.test.GuardedMACInterface;
 import org.esfinge.guardian.mac.test.Nivel;
 import org.esfinge.guardian.mac.utils.MacConfig;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SensitivityLevelTester {
-	
-	@Test(expected=AuthorizationException.class)
+
+	MacConfig macConfig;
+
+	@Before
+	public void setUp() {
+		macConfig = new MacConfig();
+	}
+
+	@Test(expected = AuthorizationException.class)
 	public void testSubjectLevelLowerThanOperationLevel() {
-		WrappedObj<Level> authLevel = 
-					AuthorizationContext.wrapAsSubjectProp(
-							MacConfig.getAuthorizationLevelKey(), Level.CLASSIFIED);
-		
+		WrappedObj<Level> authLevel = AuthorizationContext.wrapAsSubjectProp(macConfig.getAuthorizationLevelKey(),
+				Level.CLASSIFIED);
+
 		GuardedMACInterface gc = AuthorizationContext.guardObject(new GuardedMACClass(), authLevel);
-		
+
 		gc.executeDependentOfSubjectLevel();
 	}
-	
+
 	@Test
 	public void testSubjectLevelEqualsToOperationLevel() {
-		WrappedObj<Level> authLevel = 
-					AuthorizationContext.wrapAsSubjectProp(
-							MacConfig.getAuthorizationLevelKey(), Level.TOP_SECRET);
-		
+		WrappedObj<Level> authLevel = AuthorizationContext.wrapAsSubjectProp(macConfig.getAuthorizationLevelKey(),
+				Level.TOP_SECRET);
+
 		GuardedMACInterface gc = AuthorizationContext.guardObject(new GuardedMACClass(), authLevel);
-		
-		assertEquals("This method must always execute", true, gc.executeDependentOfSubjectLevel() );
+
+		assertEquals("This method must always execute", true, gc.executeDependentOfSubjectLevel());
 	}
-	
+
 	@Test
 	public void testSubjectLevelGreaterThanOperationLevel() {
-		WrappedObj<Level> authLevel = 
-					AuthorizationContext.wrapAsSubjectProp(
-							MacConfig.getAuthorizationLevelKey(), Level.CLASSIFIED);
-		
+		WrappedObj<Level> authLevel = AuthorizationContext.wrapAsSubjectProp(macConfig.getAuthorizationLevelKey(),
+				Level.CLASSIFIED);
+
 		GuardedMACInterface gc = AuthorizationContext.guardObject(new GuardedMACClass(), authLevel);
-		
-		assertEquals("This method must always execute", true, gc.execByAllSubjectsWithAnyLevel() );
+
+		assertEquals("This method must always execute", true, gc.execByAllSubjectsWithAnyLevel());
 	}
-	
-	@Test(expected=EnumNotDefinedException.class)
+
+	@Test(expected = EnumNotDefinedException.class)
 	public void testSensitivityLevelUseWithNoSubjectLevel() {
 		GuardedMACInterface gc = AuthorizationContext.guardObject(new GuardedMACClass());
-		
-		assertEquals("This method must always execute", true, gc.execByAllSubjectsWithAnyLevel() );
+
+		assertEquals("This method must always execute", true, gc.execByAllSubjectsWithAnyLevel());
 	}
-	
+
 	@Test
 	public void testSensitivityLevelUseWithNonDefaultAnnotationWithoutRestriction() {
-		WrappedObj<Nivel> authLevel = 
-				AuthorizationContext.wrapAsSubjectProp(
-						MacConfig.getAuthorizationLevelKey(), Nivel.PUBLICO);
-	
+		WrappedObj<Nivel> authLevel = AuthorizationContext.wrapAsSubjectProp(macConfig.getAuthorizationLevelKey(),
+				Nivel.PUBLICO);
+
 		GuardedMACInterface gc = AuthorizationContext.guardObject(new GuardedMACClass(), authLevel);
-		
-		assertEquals("This method must always execute", true, gc.executeByAllSubjectWithAnyLevelWithNonDefaultAnnotationWithoutRestriction() );
+
+		assertEquals("This method must always execute", true,
+				gc.executeByAllSubjectWithAnyLevelWithNonDefaultAnnotationWithoutRestriction());
 	}
-	
-	@Test(expected=AuthorizationException.class)
+
+	@Test(expected = AuthorizationException.class)
 	public void testSensitivityLevelUseWithNonDefaultAnnotationWithRestriction() {
-		WrappedObj<Nivel> authLevel = 
-				AuthorizationContext.wrapAsSubjectProp(
-						MacConfig.getAuthorizationLevelKey(), Nivel.PUBLICO);
-	
+		WrappedObj<Nivel> authLevel = AuthorizationContext.wrapAsSubjectProp(macConfig.getAuthorizationLevelKey(),
+				Nivel.PUBLICO);
+
 		GuardedMACInterface gc = AuthorizationContext.guardObject(new GuardedMACClass(), authLevel);
-		
+
 		gc.executeByAllSubjectWithAnyLevelWithNonDefaultAnnotationWithRestriction();
 	}
 }
